d7af5e4753f75c4e5ec184a0ca7aeb94e810fb54
neverNik <axwqaz@gmail.com>
2022-04-22 12:39:46 +0500

    Java 2 ДЗ от 7 урока

---
+++ src/main/java/com/example/java2/ChatClient.java
@@ -0,0 +1,78 @@
+package com.example.java2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ChatClient {
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private ClientController controller;
+
+    public ChatClient(ClientController controller) {
+        this.controller = controller;
+    }
+
+    public void openConnection() throws IOException {
+        socket = new Socket("localhost", 8189);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(() -> {
+            try {
+                waitAuth();
+                readMessage();
+            } finally {
+                closeConnection();
+            }
+        }).start();
+
+    }
+
+    private void waitAuth() {
+        while (true) {
+            try {
+                final String msg = in.readUTF(); // /authok nick
+                if (msg.startsWith("/authok")) {
+                    final String[] split = msg.split(" ");
+                    final String nick = split[1];
+                    controller.toggleBoxesVisibility(true);
+                    controller.addMessage("Успешная авторизация под ником " + nick);
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void readMessage() {
+        while (true) {
+            try {
+                String msg = in.readUTF();
+                if ("/end".equals(msg)) {
+                    controller.toggleBoxesVisibility(false);
+                    break;
+                }
+                controller.addMessage(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void closeConnection() {
+
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
